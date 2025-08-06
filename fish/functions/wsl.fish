51c396f561c0ffc8e39e424f6b7f70a75aa2fb6c
Alexander <ironsomnia@gmail.com>
2025-08-05 20:00:43 -0400

    feat: nvim wsl integration

--- fish/functions/wsl.fish
+++ fish/functions/wsl.fish
@@ -1,3 +1,17 @@
 if cat /proc/version | grep -q WSL 
     export BROWSER="/mnt/c/Windows/explorer.exe"
 end
+
+function nvim
+    if not pidof socat > /dev/null 2>&1
+        test -e /tmp/discord-ipc-0 && rm -f /tmp/discord-ipc-0
+        socat UNIX-LISTEN:/tmp/discord-ipc-0,fork \
+            EXEC:"npiperelay.exe //./pipe/discord-ipc-0" 2>/dev/null &
+    end
+
+    if test (count $argv) -eq 0
+        command nvim
+    else
+        command nvim "$argv"
+    end
+end
