ad6164b098bcf8535d3c7965506367179953d601
Alexander <ironsomnia@gmail.com>
2025-08-25 05:38:27 -0400

    chore: remove useless escape pairs keymap

--- nvim/.config/nvim/lua/remap.lua
+++ nvim/.config/nvim/lua/remap.lua
@@ -14,29 +14,6 @@ vim.keymap.set("v", "<Down>", "<nop>")
 vim.keymap.set("v", "<Left>", "<nop>")
 vim.keymap.set("v", "<Right>", "<nop>")
 
-function EscapePair()
-    local closers = { ")", "]", "}", ">", "'", "\"", "`", "," }
-    local line = vim.api.nvim_get_current_line()
-    local row, col = table.unpack(vim.api.nvim_win_get_cursor(0))
-    local after = line:sub(col + 1, -1)
-    local closer_col = #after + 1
-    local closer_i = nil
-    for i, closer in ipairs(closers) do
-        local cur_index, _ = after:find(closer)
-        if cur_index and (cur_index < closer_col) then
-            closer_col = cur_index
-            closer_i = i
-        end
-    end
-    if closer_i then
-        vim.api.nvim_win_set_cursor(0, { row, col + closer_col })
-    else
-        vim.api.nvim_win_set_cursor(0, { row, col + 1 })
-    end
-end
-
-vim.keymap.set("i", "<C-l>", EscapePair)
-
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
 
