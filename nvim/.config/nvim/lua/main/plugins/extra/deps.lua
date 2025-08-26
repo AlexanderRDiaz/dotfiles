d2145ecc15f1c04245bf2996e3be77c0b772481d
Alexander <ironsomnia@gmail.com>
2025-08-26 04:01:24 -0400

    chore(nvim): format

--- nvim/.config/nvim/lua/main/plugins/extra/deps.lua
+++ nvim/.config/nvim/lua/main/plugins/extra/deps.lua
@@ -1,14 +1,6 @@
 return {
-    {
-        "b0o/SchemaStore.nvim",
-    },
-    {
-        "nvim-tree/nvim-web-devicons",
-    },
-    {
-        "nvim-lua/plenary.nvim",
-    },
-    {
-        "lukas-reineke/lsp-format.nvim",
-    },
+    { "b0o/SchemaStore.nvim" },
+    { "nvim-tree/nvim-web-devicons" },
+    { "nvim-lua/plenary.nvim" },
+    { "lukas-reineke/lsp-format.nvim" },
 }
--- nvim/.config/nvim/lua/main/plugins/lsp/jsonls.lua
+++ nvim/.config/nvim/lua/main/plugins/lsp/jsonls.lua
@@ -1,23 +1,21 @@
-local extra = {
-    {
-        description = "Rojo project schema",
-        fileMatch = { "*.project.json" },
-        name = "project.json",
-        url = "https://raw.githubusercontent.com/rojo-rbx/vscode-rojo/master/schemas/project.template.schema.json",
-    },
-    {
-        description = "Lua reconfiguration schema",
-        fileMatch = { ".luarc.json", ".luarc.jsonc" },
-        name = "luarc.json",
-        url = "https://raw.githubusercontent.com/sumneko/vscode-lua/master/setting/schema.json",
-    },
-}
-
 return {
     settings = {
         json = {
             schemas = require("schemastore").json.schemas {
-                extra = extra,
+                extra = {
+                    {
+                        description = "Rojo project schema",
+                        fileMatch = { "*.project.json" },
+                        name = "project.json",
+                        url = "https://raw.githubusercontent.com/rojo-rbx/vscode-rojo/master/schemas/project.template.schema.json",
+                    },
+                    {
+                        description = "Lua reconfiguration schema",
+                        fileMatch = { ".luarc.json", ".luarc.jsonc" },
+                        name = "luarc.json",
+                        url = "https://raw.githubusercontent.com/sumneko/vscode-lua/master/setting/schema.json",
+                    },
+                },
             },
             validate = { enable = true },
             format = { enable = true },
