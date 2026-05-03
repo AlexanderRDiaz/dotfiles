31b3a852219fa3041b9105ea391c1de568436034
Alexander <ironsomnia@gmail.com>
2026-05-03 19:20:22 -0400

    systemd: update services & swww -> awww

---
+++ systemd.sh
@@ -0,0 +1,4 @@
+systemctl --user enable --now --user awww-daemon.service
+systemctl --user enable --now --user cliphist.service
+systemctl --user enable --now --user swaync.service
+systemctl --user enable --now --user waybar.service
